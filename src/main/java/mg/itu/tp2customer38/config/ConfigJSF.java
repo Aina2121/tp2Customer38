11f897d533b656175cea1e95f81dfc5e5cc546fe
Andrianina12 <ramorasata.andrianina@gmail.com>
2023-04-01 23:56:20 +0300

    Exo

---
+++ src/main/java/mg/itu/tp2customer38/config/ConfigJSF.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tp2customer38.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
